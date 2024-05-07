93ff4bb42e0338489c91048272683516bbcfcf92
Gabriela Sarah Moura Silva <gabrielasarah16@hotmail.com>
2024-05-07 07:58:11 -0300

    adicionando TODO ao projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //TODO: Exibir as mensagens para o nosso usuário
+
+        //TODO: Obter pela scanner os valores digitados no terminal
+
+        //TODO: Exibir a mensagem conta criada
+
+        //TODO: Declarar as variáveis de acordo com suas regras
     }
 }
